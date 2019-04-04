f642d32a1ff3848294da50aae585831cefe7de32
chuyuxuan123 <chuyuxuan@126.com>
2019-04-04 20:50:59 +0800

    naive api implement

--- hw2/src/main/java/com/example/demo/WordladderController.java
+++ hw2/src/main/java/com/example/demo/WordladderController.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WordladderController {
 
+    Wordladder w = new Wordladder(new Dictionary("G:\\SE_420\\hw2\\src\\main\\resources\\dictionary\\smalldict1.txt"));
+
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello world";
+    }
+
+    @RequestMapping("/wordladder")
+    public String result(@RequestParam(value = "word1") String word1, @RequestParam(value = "word2") String word2) {
+        return w.solve(word1,word2).toString();
     }
 }
