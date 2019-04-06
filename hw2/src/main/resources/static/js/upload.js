b67a8a9220ee4ad8facd8acc4e6af58c66ac4394
chuyuxuan123 <chuyuxuan@126.com>
2019-04-06 17:51:46 +0800

    update front end

---
+++ hw2/src/main/resources/static/js/upload.js
@@ -0,0 +1,27 @@
+function handleClick() {
+    var url = 'http://localhost:8080/wordladder';
+
+    var word1 = document.getElementById("word1").value;
+    var word2 = document.getElementById("word2").value;
+
+    console.log(word1);
+    console.log(word2);
+
+    $.ajax({
+        url:url,
+        data:{
+          word1:word1,
+          word2:word2,
+        },
+        type:"GET",
+        success:function(data){
+            alert(data);
+        }
+
+    });
+    // $.get(`http://localhost:8080/wordladder?word1=${word1}&word2=${word2}`, function (data,status) {
+    //     console.log(data);
+    //
+    // } );
+}
+
