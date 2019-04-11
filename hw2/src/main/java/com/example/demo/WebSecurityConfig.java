8a998c6907f9af804b5d2bb34f0c7eb7cf967327
chuyuxuan123 <chuyuxuan@126.com>
2019-04-11 21:37:24 +0800

    add commit

--- hw2/src/main/java/com/example/demo/WebSecurityConfig.java
+++ hw2/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -15,21 +15,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();//spring security 5.2必须要求passwordencoder
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("Alice").password("123456").roles("USER").build());
+        manager.createUser(User.withUsername("Alice").password("123456").roles("USER").build());//这里添加了一个用户，用户名为Alice，密码123456
         return manager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/s").hasRole("USER")
+                .antMatchers("/").permitAll()//没登录的用户可以看到首页提示
+                .antMatchers("/s").hasRole("USER")//只有登录后的用户才能进入wordladder页面
                 .and().formLogin()
                 .and().httpBasic();
 
