877167fe9c7c5f5463aa3886bb96e6b0490ec50e
chuyuxuan123 <chuyuxuan@126.com>
2019-04-14 00:01:29 +0800

    spring actuator and unit test

--- hw2/src/main/java/com/example/demo/WebSecurityConfig.java
+++ hw2/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,32 +33,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()//没登录的用户可以看到首页提示
                 .antMatchers("/hello").permitAll()//供测试
                 .antMatchers("/main").hasRole("USER")//只有登录后的用户才能进入wordladder页面
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
                 .and().formLogin().defaultSuccessUrl("/main",true)//登录成功跳转到/main页面
                 .and().httpBasic();
 
+//        //仅测试时使用，访问所有(包括用户定义的api)endpoint不需要权限
+//        http.requestMatcher(EndpointRequest.to("/actuator")).authorizeRequests()
+//                .anyRequest().permitAll();
+
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest().fullyAuthenticated()
-//                .and()
-//                .formLogin();
-//    }
 
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception{
-//        auth
-//                .ldapAuthentication()
-//                .userDnPatterns("uid={0},ou=people")
-//                .groupSearchBase("ou=groups")
-//                .contextSource()
-//                    .url("ldap://localhost:8389/dc=springframework,dc=org")
-//                    .and()
-//                .passwordCompare()
-//                    .passwordEncoder(new LdapShaPasswordEncoder())
-//                    .passwordAttribute("userPassword");
-//    }
 }
